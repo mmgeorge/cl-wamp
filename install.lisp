66174282deeb5f22437f5a86d620a6561fb65267
mmgeorge <matt@mattgeorge.net>
2018-08-05 20:28:00 +0000

    .

--- install.lisp
+++ install.lisp
@@ -6,6 +6,9 @@
   (ql:add-to-init-file))
 
 ;; Add current directory
+(format t "Adding current directory to asdf ~a" (sb-posix:getcwd))
 (push (sb-posix:getcwd) asdf:*central-registry*)
+
+(format t "Adding current directory to asdf ~a" asdf:*central-registry*)
 
 
