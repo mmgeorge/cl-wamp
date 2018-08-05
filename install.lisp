69412b47545c272eb20e2eaa4acb4e25f8b9a58e
mmgeorge <matt@mattgeorge.net>
2018-08-05 22:58:30 +0000

    Caching support

--- install.lisp
+++ install.lisp
@@ -5,9 +5,21 @@
 
 (handler-case (quicklisp-quickstart:install)
   (t (e)
-    (declare (ignore e)))
+    (declare (ignore e))))
+
+(with-open-file (stream (merge-pathnames (merge-pathnames (user-homedir-pathname) ".sbclrc"))
+                        :direction :output :if-exists :append)
+  (write `(let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
+                                                 (user-homedir-pathname))))
+            (when (probe-file quicklisp-init)
+              (load quicklisp-init)))
+         :stream stream))
+  
 
 
+                                 )
+
+  
 
 ;; (if (not (probe-file *ql-path*))
 ;;     ;; Install quicklisp if it does not exist
