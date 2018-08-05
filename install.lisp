a80a552554aea6d6f3ab8686894de3cfdc85a06f
mmgeorge <matt@mattgeorge.net>
2018-08-05 22:25:21 +0000

    Caching support

--- install.lisp
+++ install.lisp
@@ -2,13 +2,21 @@
 (defvar *ql-path* (merge-pathnames (user-homedir-pathname) "quicklisp"))
 (format t "QL-PATH ~a" *ql-path*)
 
-(if (not (probe-file *ql-path*))
-    ;; Install quicklisp if it does not exist
-    (progn
-      (load "quicklisp.lisp")
-      (quicklisp-quickstart:install))
-    ;; Otherwise load the setup file
-    (load (merge-pathnames *ql-path* "setup.lisp")))
+(load "quicklisp.lisp")
+
+(handler-case (quicklisp-quickstart:install)
+  (t (e)
+    (load (merge-pathnames *ql-path* "setup.lisp"))))
+
+
+
+;; (if (not (probe-file *ql-path*))
+;;     ;; Install quicklisp if it does not exist
+;;     (progn
+      
+;;       )
+;;     ;; Otherwise load the setup file
+;;     (load (merge-pathnames *ql-path* "setup.lisp")))
 
 (ql-util:without-prompting
   (ql:add-to-init-file))
