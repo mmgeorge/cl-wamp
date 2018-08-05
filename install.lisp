2273fb03a17e0f483c6cfa24336b95eb4c9e6f7e
mmgeorge <matt@mattgeorge.net>
2018-08-05 19:28:52 +0000

    .

--- install.lisp
+++ install.lisp
@@ -2,6 +2,7 @@
 ;; Setup quicklisp
 (format t "Installing quicklisp package manager")
 (load "quicklisp.lisp")
+(setf *quickload-prompt* nil)
 (quicklisp-quickstart:install)
 (ql:add-to-init-file)
 (format t "Sucessfully installed quicklisp")
