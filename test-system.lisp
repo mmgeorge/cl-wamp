c6f64b9a84f0426e06eaada0126e7f6592afe7b8
mmgeorge <matt@mattgeorge.net>
2018-08-06 01:54:08 +0000

    CI

--- test-system.lisp
+++ test-system.lisp
@@ -1,8 +1,9 @@
 
-(let ((*standard-output* (make-string-output-stream)))
+(let ((real-stdout *standard-output*)
+      (*standard-output* (make-string-output-stream)))
   (asdf:test-system "wamp/test")
   (let ((output (get-output-stream-string *standard-output*)))
-    (format t "~a" output)
+    (format real-stdout "~a" output)
     (if (search "failed" output)
         (error "It failed!")
         (format t "It worked!"))))
