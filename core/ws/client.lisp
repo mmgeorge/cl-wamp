e728c31f84392fa85a61a2115fabfac9ec611135
mmgeorge <matt@mattgeorge.net>
2019-01-16 07:52:30 +0000

    Fix client cleanup

--- core/ws/client.lisp
+++ core/ws/client.lisp
@@ -8,16 +8,17 @@
   (:import-from :wamp/ws/session/websocket #:websocket)
   (:import-from :wamp/ws/session/session #:session)
   (:local-nicknames (:session :wamp/ws/session/session))
-  (:export #:client #:recieve #:send))
-
+  (:export #:client #:recieve #:send #:destroy))
 
 (in-package :wamp/ws/client)
 
 
-
 (defclass client ()
   ((session :reader session :initform nil)
-   (thread :accessor thread :initform nil)))
+   (main-thread :reader main-thread :initform (bt:current-thread))
+   (thread :accessor thread :initform nil)
+   ;; one-of :open, :shutting-down, or :closed
+   (status :accessor status :initform :open)))
 
 
 (defgeneric recieve (self message &key))
@@ -50,26 +51,34 @@
   (destructuring-bind (type buffer end) message
     (declare (ignore type))
     (format t "Client: You got mail! ~a~%"
-            (flexi-streams:octets-to-string buffer :start 0 :end end :external-format :utf-8)))
-  ;(format t "Got a message!~%")
-  ;(error "No default implementation for client:recieve found!~%")
-  )
+            (flexi-streams:octets-to-string buffer :start 0 :end end :external-format :utf-8))))
 
 
 (defun poll (self)
-  (with-slots (session) self
+  (with-slots (session status) self
     (loop
-      (usocket:wait-for-input session)
-      (when-let (message (session:recieve session))
-        (funcall #'recieve self message )))))
+      (when (eq status :shutting-down)
+        (usocket:socket-close (session self))
+        (setf (status self) :closed)
+        (return-from poll))
+      (when (usocket:wait-for-input session :timeout 0.1)
+        (unless (eq (Status self) :shutting-down)
+        (when-let (message (session:recieve session))
+          ;(bt:interrupt-thread (main-thread self) #'(lambda () (funcall #'recieve self message)))
+          (funcall #'recieve self message )
+          ))))))
   
 
-
 (defun destroy (self)
-  (with-slots (thread) self
-    (when (and thread (bt:thread-alive-p thread))
-      (bt:destroy-thread thread)
-      (setf thread nil))))
+  (when (eq (status self) :open)
+    (with-slots (thread) self
+      (unless (and thread (bt:thread-alive-p thread))
+        (error "Unable to destroy client. Polling thread is already destroyed~%"))
+      (setf (status self) :shutting-down)
+      (unless (eq (bt:current-thread)
+                  (thread self))
+        (loop until (eq (status self) :closed)))
+      (setf (thread self) nil))))
 
 
 (defmethod send ((self client) (message simple-array) &key)
@@ -79,6 +88,7 @@
 
 
 (defvar *client* nil)
+
 
 (defun test ()
   (when *client* (destroy *client*))
