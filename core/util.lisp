35bcbbd0d3f6b4b6aaf242fb169aadd0031b60f8
mmgeorge <matt@mattgeorge.net>
2019-03-02 19:39:00 +0000

    Remove explit slynk loading

--- core/util.lisp
+++ core/util.lisp
@@ -18,7 +18,7 @@
 
 (defun start-event-loop ()
   (progn
-    (slynk-mrepl::send-prompt slynk-mrepl:*repl*)
+    ;(slynk-mrepl::send-prompt slynk-mrepl:*repl*)
     (as:start-event-loop
      (lambda ()
        (as:add-event-loop-exit-callback
@@ -26,7 +26,8 @@
             (format t "Event loop exited..~%")))
        (as:idle
         (lambda ()
-          (slynk::handle-requests (slynk::default-connection) t)))))))
+          ;(slynk::handle-requests (slynk::default-connection) t)
+          ))))))
 
 
 (unless lparallel:*kernel*
