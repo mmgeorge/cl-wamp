a10ab1514a4461b1dfeeed427269bb281323b4e4
mmgeorge <matt@mattgeorge.net>
2019-03-02 23:16:08 +0000

    Exit with error code on test failure

--- run.lisp
+++ run.lisp
@@ -3,15 +3,12 @@
 (ql:quickload "cl-async")
 (ql:quickload "blackbird")
 
+
 (as:start-event-loop
  (lambda ()
    (bb:attach
     (expect:run-tests "wamp" t)
     (lambda (report)
-      (format t "Got failed: ~A~%" (expect/report/report:nested-failed-length report))
-      
-      (as:exit-event-loop)))
-   (as:add-event-loop-exit-callback
-    #'(lambda ()
-        (format t "Event loop exited..~%")))))
+      (as:exit-event-loop)
+      (uiop:quit (expect/report/report:nested-failed-length report))))))
 
