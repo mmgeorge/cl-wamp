e35cf163de0e50765c0a99f9a9e4d3893ab43a54
mmgeorge <matt@mattgeorge.net>
2019-03-02 19:30:17 +0000

    Add event loop exit cb

--- run.lisp
+++ run.lisp
@@ -1,10 +1,12 @@
 (ql:quickload "expect")
 (ql:quickload "wamp")
 (ql:quickload "cl-async")
+(ql:quickload "blackbird")
 
 (as:start-event-loop
  (lambda ()
-   (expect:run-tests "wamp")
+   (bb:wait (expect:run-tests "wamp")
+     (as:exit-event-loop))
    (as:add-event-loop-exit-callback
     #'(lambda ()
         (format t "Event loop exited..~%")))))
